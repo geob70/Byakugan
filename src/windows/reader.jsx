d9f629869003cd1d951a27d12ff8527a33823c48
George Sylvester Olamide <geoslyman57@gmail.com>
2020-03-17 16:35:08 +0100

    porter stemming

---
+++ src/windows/reader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {} from "@material-ui/core";
+import {} from "@material-ui/icons";
+import {} from "@material-ui/core/styles";
+
+function Reader(props) {
+  return <div></div>;
+}
+export default Reader;
